0a8aba62863f86072a100b2fc9f9d503460464ec
Mário Cao <kreativermario@gmail.com>
2023-04-07 12:06:38 +0100

    feat: server info command
    Signed-off-by: Mário Cao <kreativermario@gmail.com>

--- src/cogs/help_cog.py
+++ src/cogs/help_cog.py
@@ -30,23 +30,32 @@ class Help(commands.Cog):
             for cog in self.bot.cogs.values():
                 # Filter out hidden cogs
                 if not cog.hidden:
-                    cog_commands = [cmd for cmd in cog.get_commands() if not cmd.hidden]
+                    cog_commands = [cmd for cmd in cog.get_commands()
+                                    if not cmd.hidden]
                     if cog_commands:
                         command_list = [f"`{cmd.name}`" + (
-                            f" (aliases: {' | '.join(cmd.aliases)})" if cmd.aliases else "") for cmd in cog_commands]
-                        embed.add_field(name=cog.__cog_name__, value='\n'.join(command_list), inline=False)
+                            f" (aliases: {' | '.join(cmd.aliases)})"
+                            if cmd.aliases else "") for cmd in cog_commands]
+                        embed.add_field(name=cog.__cog_name__,
+                                        value='\n'.join(command_list),
+                                        inline=False)
         elif len(args) == 1:
             # Arg given, try to show detailed command help
             command = self.bot.get_command(args[0])
             if not command:
                 await ctx.send(f"No command found for '{args[0]}'")
                 return
-            embed = discord.Embed(title=f"{command.name.capitalize()} Command Help", description=command.help,
+            embed = discord.Embed(title=
+                                  f"{command.name.capitalize()} Command Help",
+                                  description=command.help,
                                   color=0x00ff00)
             if command.aliases:
-                embed.add_field(name='Aliases', value=' | '.join(command.aliases), inline=False)
+                embed.add_field(name='Aliases',
+                                value=' | '.join(command.aliases),
+                                inline=False)
         else:
-            await ctx.send(f"Invalid number of arguments. Usage: `{self.bot.command_prefix}help [command]`")
+            await ctx.send(f"Invalid number of arguments. "
+                           f"Usage: `{self.bot.command_prefix}help [command]`")
             return
         embed.set_footer(text="Powered by SkyWizz")
         await ctx.send(embed=embed)
