83ef53e4eaa95887044b44b06f2a8d9296ff6db6
Mário Cao <kreativermario@gmail.com>
2023-08-30 21:18:31 +0100

    refactor: removed deprecated functions and commands related to the aviation API
    Signed-off-by: Mário Cao <kreativermario@gmail.com>

--- src/cogs/utils/exceptions.py
+++ src/cogs/utils/exceptions.py
@@ -1,23 +1,3 @@
-class AirportCodeError(Exception):
-    """Base class for exceptions related to airport codes"""
-    pass
-
-
-class EmptyAirportCodeError(AirportCodeError):
-    """Raised when the airport code argument is empty"""
-    pass
-
-
-class InvalidAirportCodeError(AirportCodeError):
-    """Raised when the airport code is not a valid IATA or ICAO code"""
-    pass
-
-
-class DifferentCodeFormatError(AirportCodeError):
-    """Raised when the two airport codes are not in the same format"""
-    pass
-
-
 class APIDataError(Exception):
     """Base class for exceptions related to API data"""
     pass
--- src/cogs/utils/utility_functions.py
+++ src/cogs/utils/utility_functions.py
@@ -1,9 +1,29 @@
+import functools
+import warnings
+
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 import textwrap
 import whois
 
-from .exceptions import APIRequestError, InvalidAirportCodeError
+from .exceptions import APIRequestError
+
+
+def deprecated(func):
+    """This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emitted
+    when the function is used."""
+
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        warnings.simplefilter('always', DeprecationWarning)  # turn off filter
+        warnings.warn("Call to deprecated function {}.".format(func.__name__),
+                      category=DeprecationWarning,
+                      stacklevel=2)
+        warnings.simplefilter('default', DeprecationWarning)  # reset filter
+        return func(*args, **kwargs)
+
+    return new_func
 
 
 def check_request_status(response):
@@ -14,37 +34,7 @@ def check_request_status(response):
     """
     if not (200 <= response.status_code < 300):
         raise APIRequestError(f'Request failed with status '
-                        f'{response.status_code}: {response.text}')
-
-
-def get_airport_code_type(airport_code):
-    """
-    Function that returns which airport code type
-    :param airport_code: Airport set of code
-    :return: code: type of airport code
-    :rtype: code: str
-    """
-    code = None
-    # If it is IATA Code
-    if len(airport_code) == 3:
-        code = 'iata'
-    # If it is ICAO Code
-    elif len(airport_code) == 4:
-        code = 'icao'
-    return code
-
-
-def validate_airport_code(airport_code):
-    """
-    Functions that validades airport code
-    :param airport_code: The airport code
-    :type airport_code: str
-    :return:
-    """
-    if airport_code is None:
-        raise InvalidAirportCodeError("Airport code is not valid!"
-                                      " Try using IATA or "
-                        "ICAO code format")
+                              f'{response.status_code}: {response.text}')
 
 
 async def whois_lookup(domain):
@@ -57,25 +47,25 @@ async def whois_lookup(domain):
     **Returns:**
     - A dictionary containing the WHOIS data for the domain.
     """
-    
-    DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+    date_format = '%Y-%m-%d %H:%M:%S'
     whois_data = whois.whois(domain)
     if isinstance(whois_data.expiration_date, list):
-        expiration_date = whois_data.expiration_date[0].strftime(DATE_FORMAT) \
-        if whois_data.expiration_date[0] is not None else 'N/A'
+        expiration_date = whois_data.expiration_date[0].strftime(date_format) \
+            if whois_data.expiration_date[0] is not None else 'N/A'
     else:
         expiration_date = whois_data.expiration_date
 
     if isinstance(whois_data.creation_date, list):
-        creation_date = whois_data.creation_date[0].strftime(DATE_FORMAT)
+        creation_date = whois_data.creation_date[0].strftime(date_format)
     else:
-        creation_date = whois_data.creation_date.strftime(DATE_FORMAT) \
+        creation_date = whois_data.creation_date.strftime(date_format) \
             if whois_data.creation_date is not None else 'N/A'
 
     if isinstance(whois_data.last_updated, list):
-        last_updated = whois_data.last_updated[0].strftime(DATE_FORMAT)
+        last_updated = whois_data.last_updated[0].strftime(date_format)
     else:
-        last_updated = whois_data.last_updated.strftime(DATE_FORMAT) \
+        last_updated = whois_data.last_updated.strftime(date_format) \
             if whois_data.last_updated is not None else 'N/A'
 
     name = whois_data.name if whois_data.name is not None else 'N/A'
@@ -83,12 +73,12 @@ async def whois_lookup(domain):
     registrar = whois_data.registrar if whois_data.registrar is not None \
         else 'N/A'
     name_servers = whois_data.name_servers
-    
+
     return {
         'name': name,
         'country': country,
         'registrar': registrar,
-        'creation_date': creation_date ,
+        'creation_date': creation_date,
         'expiration_date': expiration_date,
         'last_updated': last_updated,
         'name_servers': name_servers
@@ -142,5 +132,3 @@ def caption_image(image_file, caption):
         content = img_bytes.getvalue()
 
     return content
-
-
